30d5b0f6a415420b7a137f40b72b26091315f238
riha7 <harik0725@gmail.com>
2021-09-03 19:36:26 +0530

    update 2nd parameter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
-import "./style.css";
+import React, { useState, useEffect } from 'react';
+import './style.css';
 
 export default function App() {
+  const [count, setcount] = useState(0);
+  //useEffect contain 2 parameters 
+  //1st parameter function
+  //2nd parameter dependencies
+  useEffect(() => {
+    console.log('Update',count,'times');
+  }, [count]);
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      
+      <h1>Using useEffect Hook call after return Statement executes</h1>
+      <h2>Update: {count} times</h2>
+      <button onClick={() => setcount(count + 1)}>Update</button>
     </div>
   );
 }
